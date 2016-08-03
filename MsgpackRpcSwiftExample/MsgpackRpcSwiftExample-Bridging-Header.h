bcf0ced3b87a18bb6563350c7ab441c62998fc38
y-okubo <y-okubo@nekojarashi.com>
2016-08-03 18:40:49 +0900

    Add bridging header

---
+++ MsgpackRpcSwiftExample/MsgpackRpcSwiftExample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  MsgpackRpcSwiftExample-Bridging-Header.h
+//  MsgpackRpcSwiftExample
+//
+//  Created by y-okubo on 2016/08/03.
+//  Copyright © 2016 Nekojarashi Inc. All rights reserved.
+//
+
+#ifndef MsgpackRpcSwiftExample_Bridging_Header_h
+#define MsgpackRpcSwiftExample_Bridging_Header_h
+
+#import <MPMessagePack/MPMessagePack.h>
+
+#endif /* MsgpackRpcSwiftExample_Bridging_Header_h */

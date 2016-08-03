32b29b13c357860706455639fbdcd36d7e9d93ca
y-okubo <y-okubo@nekojarashi.com>
2016-08-03 18:33:25 +0900

    Add project

---
+++ MsgpackRpcSwiftExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MsgpackRpcSwiftExample
+//
+//  Created by y-okubo on 2016/08/03.
+//  Copyright © 2016 Nekojarashi Inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
